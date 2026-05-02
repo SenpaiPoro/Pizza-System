7be81b277f74623725fce678a8d290e8f9cc46da
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-05-03 01:39:56 +0800

    index Done:

--- index.php
+++ index.php
@@ -2,5 +2,30 @@
 <?php include ('include/sidebar.php'); ?>
 <?php include ('include/topbar.php'); ?>
 
+<div class="container-fluid">
+
+    <div class="container text-center">
+    <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+    <div class="row">
+        <div class="col-md-8"> <h1><b><p class="text-black ">Choose your order </p></b></h1></div>
+        <div class="col-6 col-md-4"><p class="text-info "><b>Contact Us: 0912345678 <br> Email: Coldpizza@pizza.ph</br></b></p></div>
+    </div>
+
+    <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+    <div class="row">
+        <div class="col-6 col-md-4"><img src="promo4.png" class="img-fluid rounded float-start" alt="..."> </div>
+        <div class="col-6 col-md-4"><img src="promopic1.png" class="img-fluid rounded float-start" alt="..."></div>
+        <div class="col-6 col-md-4"><img src="promo2.png" class="img-fluid rounded float-start" alt="..."></div>
+    </div>
+
+    <!-- Columns are always 50% wide, on mobile and desktop -->
+    <div class="row">
+        <div class="col-6"><p style="color: black"><h5>Please take note that the image used is copyrighted by <br><a href="https://www.dominospizza.ph/">Dominopizza.ph.</a></h5></p></div>
+        <div class="col-6"><img src="promo3.png" class="img-fluid rounded float-start" alt="..."></div>
+    </div>
+    </div>
+
+</div>
+
 
 <?php include ('include/footer.php'); ?>
